b3ded3d2eedcb84f2274f0c6f7e63efb5fdd4f01
Roman Schmitz <roman.schmitz@adweko.com>
2020-06-07 22:39:41 +0200

    first version for testing

--- process_json.sh
+++ process_json.sh
@@ -2,7 +2,7 @@
 
 while read -r sensorData; do
   result=($(echo "$sensorData" | jq 'select(.model == "GT-WT02" and .id == 57) | .temperature_C,.humidity'))
-  echo "curl -X POST --header "Content-Type: text/plain" --header "Accept: application/json" -d "${result[0]}" "http://openhabianpi:8080/rest/items/Sensor_Weather_Temp""
-  echo "curl -X POST --header "Content-Type: text/plain" --header "Accept: application/json" -d "${result[1]}" "http://openhabianpi:8080/rest/items/Sensor_Weather_Humidity""
+  curl -X POST --header "Content-Type: text/plain" --header "Accept: application/json" -d "${result[0]}" "http://openhabianpi:8080/rest/items/Sensor_Weather_Temp"
+  curl -X POST --header "Content-Type: text/plain" --header "Accept: application/json" -d "${result[1]}" "http://openhabianpi:8080/rest/items/Sensor_Weather_Humidity"
 done
 
