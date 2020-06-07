5dab9bb5f8d5fed480730f8bc14e4287928a5c65
Roman Schmitz <roman.schmitz@adweko.com>
2020-06-07 22:36:30 +0200

    first version for testing

---
+++ process_json.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+while read -r sensorData; do
+  result=($(echo "$sensorData" | jq 'select(.model == "GT-WT02" and .id == 57) | .temperature_C,.humidity'))
+  echo "curl -X POST --header "Content-Type: text/plain" --header "Accept: application/json" -d "${result[0]}" "http://openhabianpi:8080/rest/items/Sensor_Weather_Temp""
+  echo "curl -X POST --header "Content-Type: text/plain" --header "Accept: application/json" -d "${result[1]}" "http://openhabianpi:8080/rest/items/Sensor_Weather_Humidity""
+done
+
--- rtl433_items.sh
+++ rtl433_items.sh
@@ -1,1 +1,4 @@
 #!/usr/bin/env bash
+
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+rtl_433 -F json -M protocol -M level | $SCRIPTPATH/process_json.sh
